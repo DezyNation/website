4e483e4ee6f9e17b0fa50b776c44f3419e41ce29
DezyNation <87603422+DezyNation@users.noreply.github.com>
2022-04-12 13:19:12 +0530

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -8,13 +8,13 @@ const closeBtn = document.getElementById("close-btn");
 const Partners = [
     {
         name: "Hostinger",
-        img: "assets/img/hostingerOffer.png",
-        code: "DIWALI2021",
+        img: "img/hostingerOffer.png",
+        code: "DIWALI",
         website: "https://www.hostg.xyz/aff_c?offer_id=264&aff_id=92097"
     },
     {
         name: "Hostgator",
-        img: "assets/img/hostgatorOffer.png",
+        img: "img/hostgatorOffer.png",
         code: "GATORMODEL",
         website: "https://partners.hostgator.com/dezynation"
     }
